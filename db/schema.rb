3b9e51ed9194aef67e91454ec943a884e7431001
Emmanu Varghese <emmanuvarghese@gmail.com>
2024-06-12 18:42:33 +0530

    create contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_121921) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_12_123144) do
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
